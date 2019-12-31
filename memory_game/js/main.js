9679f21d25e7f9086677c5f8481299624c96c7ae
tukrong <tukrong@gmail.com>
2019-12-31 11:54:07 -0500

    memory_game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,27 @@
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+},
+
+];
+
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -9,10 +32,21 @@ function checkForMatch() {
 	}
 }
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 }
 flipCard(0);
+console.log(cards[0].cardImage);
+console.log(cards[0].suit);
 flipCard(2);
+console.log(cards[2].cardImage);
+console.log(cards[2].suit);
+
 checkForMatch()
+if(cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You have found a match");
+}	else {
+	alert("Sorry, try again.");
+}
+
 
